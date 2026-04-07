cde5883b756034f53f22bdf8095d8c53576a075d
ramokhele <142801280+farmaid-LUCT@users.noreply.github.com>
2026-04-07 06:59:01 -0700

    Change email backend to console for development

--- farm_aid_backend/farm_aid_project/settings.py
+++ farm_aid_backend/farm_aid_project/settings.py
@@ -167,7 +167,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + ['authorization', 'content-type', '
 APPEND_SLASH = True
 
 # --- EMAIL ---
-EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
