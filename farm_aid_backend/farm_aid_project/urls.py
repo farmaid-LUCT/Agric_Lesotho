b440926fc4e46847dd2378da07568c6a95cd4324
ramokhele <142801280+farmaid-LUCT@users.noreply.github.com>
2026-05-06 01:29:57 -0700

    Uncomment static and media file serving in urls.py

--- farm_aid_backend/farm_aid_project/urls.py
+++ farm_aid_backend/farm_aid_project/urls.py
@@ -17,6 +17,7 @@
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# farm_aid_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -44,5 +45,5 @@ urlpatterns = [
 
 # Serve Media/Static files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
